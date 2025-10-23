24e67bf945cbcee11e1db6f3fb05327c9971ab51
div0enormous <div0enormous@gmail.com>
2025-10-23 20:05:25 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+API_ID = 20338805
+API_HASH = "665da84b46a96d6ad9130ef2e764730e"
+BOT_TOKEN = "8485574903:AAHDv398_cVb3tx3QZgr1346vvrejy76D4o"
+
+# ShrinkEarn API key
+SHORTENER_API = "d03893470baf9f44eef321aaef44cdceada80daf"
+
+# Admins (can manage posts, edit, etc.)
+ADMINS = [1500034181]
