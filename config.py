11473174d2d50e755ef0747ceca90f08299dff1a
div0enormous <div0enormous@gmail.com>
2025-10-23 21:56:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 API_ID = 20338805
-API_HASH = "665da84b46a96d6ad9130ef2e764730e"
-BOT_TOKEN = "8485574903:AAHDv398_cVb3tx3QZgr1346vvrejy76D4o"
+API_HASH = "665da84b46a96d6ad913************"
+BOT_TOKEN = "8485574903:AAHDv398_cVb3tx3QZ**********"
 
 # ShrinkEarn API key
-SHORTENER_API = "d03893470baf9f44eef321aaef44cdceada80daf"
+SHORTENER_API = "d03893470baf9f44eef32*********"
 
 # Admins (can manage posts, edit, etc.)
 ADMINS = [1500034181]
